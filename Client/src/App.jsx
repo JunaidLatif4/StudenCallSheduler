726278498122b29e2be70c2fd08d3a4e7d8c49bc
JunaidLatif <junaidlatif004@gmail.com>
2022-11-04 01:29:15 +0500

    Starting Project

---
+++ Client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import './App.scss';
+
+
+
+
+
+const App = () => {
+  return (
+    <>
+    <div className="App_container">
+    This is React
+    </div>
+    </>
+  );
+}
+
+export default App;
