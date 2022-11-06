b42241584238aab8cfe92e11ea8a6cd71fb59287
JunaidLatif <junaidlatif004@gmail.com>
2022-11-07 02:25:15 +0500

    Promotion API Integration  Added

--- Client/src/API/CMS.js
+++ Client/src/API/CMS.js
@@ -23,7 +23,6 @@ const GetHeroSectionDataAPI = async () => {
     }
     return resolved
 }
-
 const AddHeroSectionDataAPI = async (data) => {
     let resolved = {
         data: null,
@@ -46,7 +45,6 @@ const AddHeroSectionDataAPI = async (data) => {
     }
     return resolved
 }
-
 const UpdateHeroSectionDataAPI = async (id, data) => {
     let resolved = {
         data: null,
@@ -72,4 +70,72 @@ const UpdateHeroSectionDataAPI = async (id, data) => {
 
 
 
-export { GetHeroSectionDataAPI, AddHeroSectionDataAPI, UpdateHeroSectionDataAPI }
+const GetPromotionSectionDataAPI = async () => {
+    let resolved = {
+        data: null,
+        error: null
+    }
+
+    try {
+        const res = await axios({
+            url: "api/cms/promotion",
+            method: 'GET'
+        })
+        resolved.data = res.data
+    } catch (err) {
+        if (err.response) {
+            resolved.error = err.response.data.message
+        } else {
+            resolved.error = "SomeThing Went Wrong"
+        }
+    }
+    return resolved
+}
+const AddPromotionSectionDataAPI = async (data) => {
+    let resolved = {
+        data: null,
+        error: null
+    }
+
+    try {
+        const res = await axios({
+            url: "api/cms/promotion",
+            method: 'POST',
+            data
+        })
+        resolved.data = res.data
+    } catch (err) {
+        if (err.response) {
+            resolved.error = err.response.data.message
+        } else {
+            resolved.error = "SomeThing Went Wrong"
+        }
+    }
+    return resolved
+}
+const UpdatePromotionSectionDataAPI = async (id, data) => {
+    let resolved = {
+        data: null,
+        error: null
+    }
+
+    try {
+        const res = await axios({
+            url: `api/cms/promotion/${id}`,
+            method: 'PATCH',
+            data
+        })
+        resolved.data = res.data
+    } catch (err) {
+        if (err.response) {
+            resolved.error = err.response.data.message
+        } else {
+            resolved.error = "SomeThing Went Wrong"
+        }
+    }
+    return resolved
+}
+
+
+
+export { GetHeroSectionDataAPI, AddHeroSectionDataAPI, UpdateHeroSectionDataAPI, GetPromotionSectionDataAPI, AddPromotionSectionDataAPI, UpdatePromotionSectionDataAPI }
--- Client/src/Pages/Dashboard/Components/HomePageCMS/Components/PromotionSection/PromotionSection.jsx
+++ Client/src/Pages/Dashboard/Components/HomePageCMS/Components/PromotionSection/PromotionSection.jsx
@@ -3,71 +3,165 @@ import { useLocation, useNavigate } from "react-router-dom";
 
 import TextField from "@mui/material/TextField";
 import TextareaAutosize from "@mui/material/TextareaAutosize";
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
 
 import upload from "../../../../../../Assets/upload.svg";
+
+import { AddPromotionSectionDataAPI, GetPromotionSectionDataAPI, UpdatePromotionSectionDataAPI } from "../../../../../../API/CMS";
+import { toast } from "react-toastify"
 
 import "../../HomePageCMS.scss";
 
 const PromotionSection = () => {
-  let Navigate = useNavigate();
-  let location = useLocation();
+  let Navigate = useNavigate()
+  let location = useLocation()
   const fileuploadref = useRef(null);
 
-  let id = new URLSearchParams(location.search).get("id");
-
-  let [selectedTab, setSelectedTab] = useState("Add Product");
+  const [fullPageLoading, setFullPageLoading] = useState(true)
+  const [loading, setLoading] = useState(false)
   const [enteredData, setEnteredData] = useState({
-    line1: "",
-    line2: "",
-    line3: "",
-    detail: "",
-    img: null,
-  });
+    line1: '',
+    line2: '',
+    line3: '',
+    details: '',
+    video: null
+  })
+
 
   const enteringData = (event) => {
-    let { name, value } = event.target;
+    let { name, value } = event.target
 
     setEnteredData((preVal) => {
       return {
         ...preVal,
-        [name]: value,
-      };
-    });
-  };
+        [name]: value
+      }
+    })
+
+  }
 
   const onupload = () => {
     fileuploadref.current.click();
   };
   const uploadFile = async (event) => {
-    let file = event.target.files[0];
-
-    const toBase64 = (file) =>
-      new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = () => resolve(reader.result);
-        reader.onerror = (error) => reject(error);
-      });
-    let stringFile = await toBase64(file);
+    let file = event.target.files[0]
 
     setEnteredData((preVal) => {
       return {
         ...preVal,
-        img: stringFile,
-      };
-    });
-  };
+        video: {
+          link: URL.createObjectURL(file),
+          file: file
+        }
+      }
+    })
+  }
 
-  // useEffect(() => {
-  //   if (id) {
-  //     setFullPageLoading(true)
-  //     getProducts()
-  //   }
 
-  // }, [location.search])
+  const AddSectionData = async () => {
+    setLoading(true)
+    let formData = new FormData()
+    let res;
+    let data = enteredData
+
+    if (data.img && data.img.file) {
+      formData.append("video", data.video.file)
+      data.img = true
+    }
+    Object.keys(data).map((key) => {
+      if (key != "_id") {
+        if (key == "video") {
+          formData.append(key, JSON.stringify(data[key]))
+        } else {
+          formData.append(key, data[key])
+        }
+      }
+    })
+    console.log(enteredData);
+    if (data._id) {
+      res = await UpdatePromotionSectionDataAPI(data._id, formData)
+      if (res.error != null) {
+        toast.error(res.error, {
+          position: "top-right",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      } else {
+        toast.success(res.data.message, {
+          position: "top-right",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+        gettingHeroSectionData()
+      }
+    } else {
+      res = await AddPromotionSectionDataAPI(formData)
+      if (res.error != null) {
+        toast.error(res.error, {
+          position: "top-right",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      } else {
+        toast.success(res.data.message, {
+          position: "top-right",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+        gettingHeroSectionData()
+      }
+    }
+    setLoading(false)
+  }
+
+  const gettingHeroSectionData = async () => {
+    setFullPageLoading(true)
+    const res = await GetPromotionSectionDataAPI()
+    setFullPageLoading(false)
+    if (res.error != null) {
+      toast.error(res.error, {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    } else {
+      setEnteredData(res.data.data)
+    }
+  }
+
+  useEffect(() => {
+    gettingHeroSectionData()
+  }, [])
 
   return (
     <>
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={fullPageLoading}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
       <input
         ref={fileuploadref}
         style={{ display: "none" }}
@@ -116,18 +210,18 @@ const PromotionSection = () => {
               minRows={10}
               placeholder="Details"
               style={{ width: 450 }}
-              name="detail"
+              name="details"
               onChange={enteringData}
-              value={enteredData.detail}
+              value={enteredData.details}
             />
           </div>
           <div className="box_main">
             <label>Upload Video</label>
             <div className="upload_box" onClick={onupload}>
-              {enteredData.img ? (
+              {enteredData.video ? (
                 <>
                   <video controls className="uploaded_img">
-                    <source src={enteredData.img} type="video/mp4" />
+                    <source src={enteredData.video.link} type="video/mp4" />
                   </video>
                 </>
               ) : (
@@ -142,9 +236,7 @@ const PromotionSection = () => {
           </div>
         </div>
         <div className="btn_sec">
-          <button>Cancel</button>
-
-          <button>Save</button>
+          <button onClick={AddSectionData}>Save</button>
         </div>
       </div>
     </>
